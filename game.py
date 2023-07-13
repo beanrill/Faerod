7a90a3261b0a281b30a52a6287dddecfcba3bd3f
Bianca Davies <47017874+beanrill@users.noreply.github.com>
2023-07-12 20:54:36 -0700

    Added choose character function

--- game.py
+++ game.py
@@ -1,6 +1,33 @@
 
 # CONSTANTS
 
+def intro_art():
+    print(r"""
+                   ,   ,
+                 ,-`{-`/
+              ,-~ , \ {-~~-,
+            ,~  ,   ,`,-~~-,`,
+          ,`   ,   { {      } }                                             }/
+         ;     ,--/`\ \    / /                                     }/      /,/
+        ;  ,-./      \ \  { {  (                                  /,;    ,/ ,/
+        ; /   `       } } `, `-`-.___                            / `,  ,/  `,/
+         \|         ,`,`    `~.___,---}                         / ,`,,/  ,`,;
+          `        { {                                     __  /  ,`/   ,`,;
+                /   \ \                                 _,`, `{  `,{   `,`;`
+               {     } }       /~\         .-:::-.     (--,   ;\ `,}  `,`;
+               \\._./ /      /` , \      ,:::::::::,     `~;   \},/  `,`;     ,-=-
+                `-..-`      /. `  .\_   ;:::::::::::;  __,{     `/  `,`;     {
+                           / , ~ . ^ `~`\:::::::::::<<~>-,,`,    `-,  ``,_    }
+                        /~~ . `  . ~  , .`~~\:::::::;    _-~  ;__,        `,-`
+               /`\    /~,  . ~ , '  `  ,  .` \::::;`   <<<~```   ``-,,__   ;
+              /` .`\ /` .  ^  ,  ~  ,  . ` . ~\~                       \\, `,__
+             / ` , ,`\.  ` ~  ,  ^ ,  `  ~ . . ``~~~`,                   `-`--, \
+            / , ~ . ~ \ , ` .  ^  `  , . ^   .   , ` .`-,___,---,__            ``
+          /` ` . ~ . ` `\ `  ~  ,  .  ,  `  ,  . ~  ^  ,  .  ~  , .`~---,___
+        /` . `  ,  . ~ , \  `  ~  ,  .  ^  ,  ~  .  `  ,  ~  .  ^  ,  ~  .  `-,
+
+                    """)
+
 
 def HP():
     return 30
@@ -8,6 +35,18 @@ def HP():
 
 def MINOTAUR_HIT():
     return 5
+
+
+def HYDRA_HIT():
+    return 10
+
+
+def CHIMERA_HIT():
+    return 6
+
+
+def CYCLOPS_HIT():
+    return 8
 
 
 # ENEMIES
@@ -42,6 +81,33 @@ def board():
     pass
 
 
+def choose_char():
+    """
+    Prompts user for their choice of character.
+    :return: Returns a string
+    """
+    while True:
+        choice = input("Pick your character: \n 1 - Wizard \n 2 - Mage \n 3 - Sorcerer \n 4 - Knight \n")
+        if choice == 1:
+            choice = "Wizard"
+            print(f"Welcome, {choice}! Get ready for an adventure.")
+            break
+        elif choice == 2:
+            choice = "Mage"
+            print(f"Welcome, {choice}! Get ready for an adventure.")
+            break
+        elif choice == 3:
+            choice = "Sorcerer"
+            print(f"Welcome, {choice}! Get ready for an adventure.")
+            break
+        elif choice == 4:
+            choice = "Knight"
+            print(f"Welcome, {choice}! Get ready for an adventure.")
+            break
+
+    return choice
+
+
 def player(name, hp, character, location, ):
     print('\033[96m' + f"\nName: {name} \n"
                        f"HP: {hp} \n"
@@ -64,10 +130,12 @@ def intro_narrative():
 
 
 def main():
-    introduction()
-    # Narrative
-    player_name = input("What would you like to be called? ")
-    player(player_name, HP(), "Mage", 5)
+    # intro_art()
+    # introduction()
+    # # Narrative
+    # player_name = input("What would you like to be called? ")
+    # player(player_name, HP(), choose_char(), 5)
+    choose_char()
 
 
 if __name__ == "__main__":
