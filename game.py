45c5e4d53c36bd543deeb4befd9da27a01fc108b
Bianca Davies <47017874+beanrill@users.noreply.github.com>
2023-08-01 21:18:10 -0700

    Comment out print statements

--- game.py
+++ game.py
@@ -252,7 +252,7 @@ def write_to_txt_file(file_path, str_to_write):
     try:
         with open(file_path, 'w') as file:
             file.write(str_to_write)
-        print("File written successfully.")
+        # print("File written successfully.")
     except Exception as e:
         print(f"Error occurred while writing to the file: {e}")
 
@@ -271,28 +271,30 @@ def read_from_txt_file(file_path):
 
 def run_microservice():
     # Write the request over
-    print("Writing 'request' to rng_pipe.txt")
+    # print("Writing 'request' to rng_pipe.txt")
     write_to_txt_file("rng_pipe.txt", "request")
 
     while (check_txt_file_contents("rng_pipe.txt", "request") or
            check_txt_file_contents("rng_pipe.txt", "")):
-        print("Waiting 1 second before checking the pipe...")
+        # print("Waiting 1 second before checking the pipe...")
         time.sleep(1)
 
     pipe_contents = read_from_txt_file("rng_pipe.txt")
     rng_num = int(pipe_contents)
 
     # Now you can use rng_num for your service!
-    print(f"Received {rng_num} from the rng_pipe.txt file")
-    print("Goodbye!")
+    # print(f"Received {rng_num} from the rng_pipe.txt file")
+    # print("Goodbye!")
 
     # Return the rng_num to use for monster generator
+    print(rng_num)
 
 # ------------------------------------------------- RUN GAME ------------------------------------------------- #
 
 
 def main():
     run_microservice()
+
     # # Introduction and instructions
     # intro_art()
     # introduction()
